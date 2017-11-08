6d09065373387b8878850d11e5084e7403a515c7
Javier Tkaez <jdtkaez@msn.com>
2017-11-07 20:57:29 -0800

    Modified java application.

--- src/packt/jee/eclipse/git/GitTestApplication.java
+++ src/packt/jee/eclipse/git/GitTestApplication.java
@@ -5,6 +5,7 @@ public class GitTestApplication {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("\nHello Git!\n");
+		System.out.println("This is my first Git change!\n");
 	}
 
 }
