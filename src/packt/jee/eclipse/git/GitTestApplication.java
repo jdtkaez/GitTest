aa34bffbe68a460449799d39efeb24e8683a3f88
Javier Tkaez <jdtkaez@msn.com>
2017-11-07 20:53:09 -0800

    First Git check-in.

---
+++ src/packt/jee/eclipse/git/GitTestApplication.java
@@ -0,0 +1,10 @@
+package packt.jee.eclipse.git;
+
+public class GitTestApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\nHello Git!\n");
+	}
+
+}
